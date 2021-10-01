1f860754d1d66f91ddff6577f9e5671a3963dce7
Andrey Zhuk <a.zhuk@bsl.dev>
2021-10-01 18:40:16 +0300

    add pitstop time counter

--- src/components/pages/laps-counter/card-robot/RobotTime.tsx
+++ src/components/pages/laps-counter/card-robot/RobotTime.tsx
@@ -1,8 +1,11 @@
-import React, { FunctionComponent } from 'react';
+import React, { FunctionComponent, useState, useEffect } from 'react';
 
 import { msToTime } from '../../../../utils';
 import { IRaceState, IRobot } from '../../../../store/race/interfaces';
+import { setTimeout } from 'timers';
 
+// Interval - component render time
+const interval: number = 10
 
 interface RobotTimeProps {
   time: IRobot['time'],
@@ -11,13 +14,39 @@ interface RobotTimeProps {
 }
 
 const RobotTime: FunctionComponent<RobotTimeProps> = (props) => {
+  const [timeInMs, setTime] = useState<number>(0);
+  const [sync, setSync] = useState<boolean>(false);
+
+  // synchronize time
+  useEffect(() => {
+    setSync(true)
+  }, [props.pitStopFinishTime, props.race.time])
+
+  useEffect(() => {
+    if (props.pitStopFinishTime - props.race.time - timeInMs > 0) {
+      setTimeout(() => {
+        if (sync) {
+          setTime(0)
+          setSync(false)
+        } else {
+          setTime(timeInMs + interval)
+        }
+      }, interval)
+    }
+  })
+
   return (
     <div className="race-table-cell time">
       <div>
         <span className="mobile-view">Time:&nbsp;</span>
         <p className="line"><span>{msToTime(props.time)}</span></p>
         <span className="mobile-view">Pitstop:&nbsp;</span>
-        <p className="line"><span>Pitstop:{(props.pitStopFinishTime - props.race.time) > 0 ? msToTime(props.pitStopFinishTime - props.race.time):'-'}</span></p>
+        <p className="line"><span>Pitstop:{
+            (props.pitStopFinishTime - props.race.time - timeInMs) > 0
+              ?  msToTime(props.pitStopFinishTime - props.race.time - timeInMs)
+              : '-'
+          }
+          </span></p>
       </div>
     </div>
   );
