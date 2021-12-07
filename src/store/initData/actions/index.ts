8cf2b200144dbf0c65a4aa55fd2c63f66af1c465
vasaeps3 <vasaeps3@gmail.com>
2021-12-07 20:16:28 +0300

    removed old

--- src/store/initData/actions/index.ts
+++
@@ -1,6 +0,0 @@
-import { submit } from "redux-form";
-
-export const LOAD_INITDATA_WS = 'redux-form-examples/account/LOAD';
-
-export const loadInitWsData = (formData) => ({ type: LOAD_INITDATA_WS, payload: formData });
-export const submitWS = () => submit('wsForm');
